9ae06d0ba3ca5decdf8cb3015e8e8faca0cfa8f3
bighong <bighong12@gmail.com>
2026-02-12 02:09:07 +0900

    DamagedConditionSO 추가

---
+++ GameDevPractice/Assets/Scripts/Control/State/scriptable object/condition/DamagedConditionSO.cs
@@ -0,0 +1,43 @@
+using System;
+using TH.Attribute;
+using TH.Combat;
+using TH.Control.State;
+using TH.Utils;
+using UnityEngine;
+
+namespace TH.Control.Data
+{
+    [CreateAssetMenu(fileName = "DamagedConditionSO", menuName = "Scriptable Objects/State Condition/DamagedConditionSO")]
+    public sealed class DamagedConditionSO : ActionStateConditionSO
+    {
+        [Header("Damage Filter")]
+        [SerializeField, Min(0f)] private float minDamage = 0f;
+        [SerializeField] private bool ignoreWhenDead = true;
+
+        public override IDisposable Bind(IActionStateController controller, Action onTriggered)
+        {
+            if (!controller.IsNotNull() || onTriggered == null)
+                return base.Bind(controller, onTriggered);
+            if (!controller.Components.TryGet(out Health health))
+                return base.Bind(controller, onTriggered);
+
+            health.OnDamaged += HandleDamaged;
+
+            return new DisposableDelegate(() =>
+            {
+                if (health.IsNotNull())
+                    health.OnDamaged -= HandleDamaged;
+            });
+
+            void HandleDamaged(in HitResult hitResult)
+            {
+                if (ignoreWhenDead && health.IsDead)
+                    return;
+                if (hitResult.Damage < minDamage)
+                    return;
+
+                onTriggered.Invoke();
+            }
+        }
+    }
+}
